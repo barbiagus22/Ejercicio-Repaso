466a533e89ee2ca2e1c59132f0693db4235f35b0
Barbara Insaurralde <40776592+barbiagus22@users.noreply.github.com>
2018-09-01 12:57:49 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,84 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- Al presionar el bot&oacute;n pedir  n&uacute;meros  hasta que el usuario quiera, mostrar:</br>
-					1-Suma de los negativos.
-					2-Suma de los positivos.
-					3-Cantidad de positivos.
-					4-Cantidad de negativos.
-					5-Cantidad de ceros.
-					6-Cantidad de números pares.
-					7-Promedio de positivos.
-					8-Promedios de negativos.
-					9-Diferencia entre positivos y negativos, (positvos-negativos).
+int division (float numero1, float numero2, float resultado);
 
-*/
 int main()
 {
-    int numero;
-    char respuesta='s';
-    int sumaNegativos=0;
-    int sumaPositivos=0;
-    int cantidadCeros=0;
-    int cantidadPositivos=0;
-    int cantidadNegativos=0;
-    int cantidadPares=0;
-    float promedioPositivos=0;
-    float promedioNegativos=0;
-    int DiferenciaPyn;
 
-    do
-    {
-        printf("Ingrese un numero: \n");
-        scanf ("%d", &numero);
+int primerResultado;
+float resultado, num1=10, num2=1.5;
 
-        if (numero<0)
-        {
 
-            sumaNegativos=sumaNegativos+numero;
-            cantidadNegativos++;
-        }
-        else if (numero>0)
-        {
 
-            sumaPositivos=sumaPositivos+numero;
-            cantidadNegativos++;
-        }
-        else
-        {
+    primerResultado = division(num1, num2, &resultado);
 
-            cantidadCeros++;
+if (primerResultado ==0 ) {
+
+    printf ("Division exitosa");
+}
+
+    else {
+        printf( "No se puede dividir por 0");
+    }
+
+
+    return 0;
+}
+
+int division (float numero1, float numero2, float resultado){
+
+        int retorno= -1;
+        float resultado;
+
+        if (numero2 != 0) {
+
+            resultado= numero1/numero2;
+            retorno=0;
         }
 
-        if  ( numero%2==0)
-        {
-
-            cantidadPares++;
-        }
-
-
-        printf(" Desea continuar si o no:  \n");
-        fflush(stdin);
-        scanf("%c", &respuesta);
-
-
-    }while (respuesta== 's');
-
-    promedioPositivos=sumaPositivos/cantidadPositivos;
-    promedioNegativos=sumaNegativos/cantidadNegativos;
-    DiferenciaPyn=cantidadPositivos-cantidadNegativos;
-
-
-    printf( " La suma de los negativos es: %d \n", sumaNegativos);
-    printf( " La suma de los positivos es: %d \n", sumaPositivos);
-    printf( " La cantidad de ceros es %d: \n", cantidadCeros);
-    printf( " La cantidad de positivos es: %d \n", cantidadPositivos);
-    printf( " La cantidad de negativos es: %d \n", cantidadNegativos);
-    printf( " La cantidad de numeros pares es: %d \n", cantidadPares);
-    printf(" El promedio de los positivos es : %f\n", promedioPositivos);
-    printf(" El promedio de los negativos es : %f\n", promedioNegativos);
-    printf(" La diferencia entre positivos y negativos : %d\n", DiferenciaPyn);
-
+        return retorno;
 
 }
